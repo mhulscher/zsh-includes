ad333c09ab87e8f6c84ea7497f61b77daafe3c66
Mitch Hulscher <mitch.hulscher@nepworldwide.nl>
2016-11-18 13:30:25 +0100

    master

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Add the following to your .zshrc:
 ```
 source cluster-mgmt.zsh
 ```
+
+# Requirements
+
+You will need the following tools in your path:
+
+* `kubectl`
+* `deis-cli`
+* `stern`
--- cluster-mgmt.zsh
+++ cluster-mgmt.zsh
@@ -16,31 +16,3 @@ function use-cluster {
     alias stern="stern --kube-config=${KUBECONFIG} -s 1s"
   fi
 }
-
-function knsmon {
-  emulate bash
-
-  COLS=$(($(tput cols)-6))
-
-  NAMESPACE=${1:-kube-system}
-  DEFAULT_OBJECTS="services daemonsets deployments pods replicasets replicationcontrollers"
-  OPTS="${@:2}"
-
-  if [[ $OPTS =~ ^\+ ]]; then
-    OPTS=$(echo ${OPTS} | sed 's/^\+//')
-    OBJECTS="$OPTS $DEFAULT_OBJECTS"
-  else
-    OBJECTS=${OPTS:-'services daemonsets deployments pods replicasets replicationcontrollers'}
-  fi
-
-  printf "=%.0s" $(seq 1 $(($COLS-${#NAMESPACE})))
-  printf " ${NAMESPACE} ===\n"
-
-  for OBJECT in ${OBJECTS}; do
-    printf "=== $OBJECT "
-    printf "=%.0s" $(seq 1 $(($COLS-${#OBJECT})))
-    printf "\n"
-    kubectl "--namespace=${NAMESPACE}" get ${OBJECT} --show-all
-    printf "\n"
-  done
-}
