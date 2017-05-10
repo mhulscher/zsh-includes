f988fd0366c54942a5f5d404b60938d5a733ee18
Mitch Hulscher <mitch.hulscher@lib.io>
2017-05-10 16:06:36 +0200

    master: no long alias stern

--- cluster-mgmt.zsh
+++ cluster-mgmt.zsh
@@ -23,12 +23,10 @@ function use-cluster {
     echo "Cluster exports cleared"
     unset KUBECONFIG
     unset DEIS_PROFILE
-    unalias stern
   else
     echo "Using cluster '${1}'"
     cd ~/clusters/${1}
     export KUBECONFIG="${HOME}/.kube/${1}"
     export DEIS_PROFILE=${1}
-    alias stern="stern --kube-config=${KUBECONFIG}"
   fi
 }
