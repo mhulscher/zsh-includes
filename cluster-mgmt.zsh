7186c541ea16279f0cabc9820e2ed0ed7b2abb94
Mitch Hulscher <mhulscher@schubergphilis.com>
2018-04-15 10:04:09 +0200

    disable use-context and use-cluster functions

--- cluster-mgmt.zsh
+++ cluster-mgmt.zsh
@@ -22,37 +22,37 @@ alias apptopcpu='watch -t "kubectl top pods --namespace=`basename $PWD` | sort -
 alias apps-from-deis="deis apps | grep -v '=== Apps' | xargs mkdir -v 2>/dev/null"
 alias apps-from-namespaces="kubectl get namespaces -ojsonpath='{.items[*].metadata.name}' | xargs -n1 mkdir -pv"
 
-function use-context {
-  [ -z ${1+x} ] && return 1
-  local q=${1}
-  local ctxt=$(kubectl config get-contexts | sed -e 1d | sed 's,\*,,' | awk '{print $1}' | grep ${q} | head -n1)
-
-  if [ "${ctxt}x" = "x" ]; then
-    echo >&2 "context not found"
-    return 1
-  fi
-
-  # echo "Switching cluster-context to '${ctxt}'"
-  # alias kubectl="kubectl --context=${ctxt}"
-  kubectl config use-context ${ctxt}
-  export DEIS_PROFILE=${ctxt}
-
-  mkdir -pv ~/clusters/${ctxt}
-  cd ~/clusters/${ctxt}
-}
-
-function use-cluster {
-  if [[ ${#@} -eq 0 ]]; then
-    echo "Cluster exports cleared"
-    unset KUBECONFIG
-    unset DEIS_PROFILE
-  else
-    echo "Using cluster '${1}'"
-    cd ~/clusters/${1}
-    export KUBECONFIG="${HOME}/.kube/${1}"
-    export DEIS_PROFILE=${1}
-  fi
-}
+#function use-context {
+#  [ -z ${1+x} ] && return 1
+#  local q=${1}
+#  local ctxt=$(kubectl config get-contexts | sed -e 1d | sed 's,\*,,' | awk '{print $1}' | grep ${q} | head -n1)
+#
+#  if [ "${ctxt}x" = "x" ]; then
+#    echo >&2 "context not found"
+#    return 1
+#  fi
+#
+#  # echo "Switching cluster-context to '${ctxt}'"
+#  # alias kubectl="kubectl --context=${ctxt}"
+#  kubectl config use-context ${ctxt}
+#  export DEIS_PROFILE=${ctxt}
+#
+#  mkdir -pv ~/clusters/${ctxt}
+#  cd ~/clusters/${ctxt}
+#}
+#
+#function use-cluster {
+#  if [[ ${#@} -eq 0 ]]; then
+#    echo "Cluster exports cleared"
+#    unset KUBECONFIG
+#    unset DEIS_PROFILE
+#  else
+#    echo "Using cluster '${1}'"
+#    cd ~/clusters/${1}
+#    export KUBECONFIG="${HOME}/.kube/${1}"
+#    export DEIS_PROFILE=${1}
+#  fi
+#}
 
 function kgetc { kubectl -n ${1} get po/${2} -o json | jq -Mr ".spec.containers[].name" }
 
